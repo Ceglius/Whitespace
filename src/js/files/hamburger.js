1fb08277e2be48f38493e6edd4291ee378a431f8
DESKTOP-936A7SQ\Tomas-PC <tomas.pateckis@gmail.com>
2023-05-31 22:18:16 +0100

    overlay for active mobile menu

--- src/js/files/hamburger.js
+++ src/js/files/hamburger.js
@@ -2,23 +2,24 @@ function burgerMenu() {
   const burger = document.querySelector(".burger");
   const menu = document.querySelector(".menu");
   const body = document.querySelector("body");
+  const overlay = document.querySelector('.overlay');
 
   burger.addEventListener("click", () => {
     if (!menu.classList.contains("active")) {
       menu.classList.add("active");
       burger.classList.add("active");
       body.classList.add("locked");
+      overlay.classList.add("active");
     } else {
       menu.classList.remove("active");
       burger.classList.remove("active");
       body.classList.remove("locked");
+      overlay.classList.remove("active");
     }
   });
 
   window.addEventListener("resize", () => {
-    if (window.innerWidth > 991.98) {
-      menu.classList.remove("active");
-      burger.classList.remove("active");
+    if (window.innerWidth > 767.98) {
       body.classList.remove("locked");
     }
   });
