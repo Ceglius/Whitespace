6316a1b08199e88e99ede2f52001c556f5396ffb
DESKTOP-936A7SQ\Tomas-PC <tomas.pateckis@gmail.com>
2023-06-02 10:34:04 +0100

    hamburger adjustments for design

--- src/js/files/hamburger.js
+++ src/js/files/hamburger.js
@@ -3,6 +3,7 @@ function burgerMenu() {
   const menu = document.querySelector(".menu");
   const body = document.querySelector("body");
   const overlay = document.querySelector('.overlay');
+  const headerBurger = document.querySelector('.header__burger');
 
   burger.addEventListener("click", () => {
     if (!menu.classList.contains("active")) {
@@ -10,17 +11,23 @@ function burgerMenu() {
       burger.classList.add("active");
       body.classList.add("locked");
       overlay.classList.add("active");
+      headerBurger.classList.add("active");
     } else {
       menu.classList.remove("active");
       burger.classList.remove("active");
       body.classList.remove("locked");
       overlay.classList.remove("active");
+      headerBurger.classList.remove("active");
     }
   });
 
   window.addEventListener("resize", () => {
     if (window.innerWidth > 767.98) {
+      menu.classList.remove("active");
+      burger.classList.remove("active");
       body.classList.remove("locked");
+      overlay.classList.remove("active");
+      headerBurger.classList.remove("active");
     }
   });
 }
