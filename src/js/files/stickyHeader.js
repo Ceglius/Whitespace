1e4dfe3ff27a10b9bff85d0a274a9f98f1dbb356
DESKTOP-936A7SQ\Tomas-PC <tomas.pateckis@gmail.com>
2023-06-04 10:09:51 +0100

    blog section html + images

--- src/js/files/stickyHeader.js
+++ src/js/files/stickyHeader.js
@@ -1,16 +1,20 @@
-const header = document.querySelector('.header');
-let scrollPosition = window.pageYOffset;
-let previousScrollPosition = scrollPosition;
+function stickyHeader() {
+  const header = document.querySelector(".header");
+  let scrollPosition = window.pageYOffset;
+  let previousScrollPosition = scrollPosition;
 
-window.addEventListener('scroll', function() {
-  const currentScrollPosition = window.pageYOffset;
+  window.addEventListener("scroll", function() {
+    const currentScrollPosition = window.pageYOffset;
 
-  if (currentScrollPosition < previousScrollPosition) {
-    // Scrolling down
-    header.classList.remove('hidden');
-  } else {
-    // Scrolling up
-    header.classList.add('hidden');
-  }
-  previousScrollPosition = currentScrollPosition;
-});
+    if (currentScrollPosition < previousScrollPosition) {
+      // Scrolling down
+      header.classList.remove("hidden");
+    } else {
+      // Scrolling up
+      header.classList.add("hidden");
+    }
+    previousScrollPosition = currentScrollPosition;
+  });
+}
+
+stickyHeader()
